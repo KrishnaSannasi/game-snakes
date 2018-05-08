b0f58a926c6b9247c930f4237292d38593bb4496
Ozaren <krishna.sd.2012@gmail.com>
2018-05-08 02:13:21 -0400

    create game window, start up, and handle input

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,34 @@ mod draw;
 mod snake;
 mod game;
 
+use piston_window::*;
+use piston_window::types::Color;
+
+use game::Game;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = (20u32, 20u32);
+
+    let mut window: PistonWindow = WindowSettings::new(
+        "Snake",
+        [width, height]
+    ).exit_on_esc(true)
+        .build()
+        .unwrap();
+    let mut game = Game::new(width, height);
+
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+
+        window.draw_2d(&event, |c, g| {
+            clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        event.update(|arg| game.update(arg.dt));
+    }
 }
